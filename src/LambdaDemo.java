a763b711f59b01c88899c8f6ee5d0b93dec3facd
Biplav Poudel <biplavpoudel764@gmail.com>
2024-01-23 14:26:42 +0545

    lambda demo

--- src/LambdaDemo.java
+++ src/LambdaDemo.java
@@ -1,14 +1,18 @@
+//To use a lambda expression in a method (in my example, printFormattedLambda())...
+// ...the method should have a parameter with a single-method interface as its type.
+// Calling the interface's method runs the lambda expression
+
 public class LambdaDemo {
-    interface StringFunction {
-        String run(String str);
+    public interface StringFunction {
+        String run(String str);  //this abstract function is defined by lambda expressions below
     }
     public static void main(String[] args) {
-        StringFunction exclaim = (s) -> s + "!";
+        StringFunction exclaim = (s) -> s + "!";   //perfectly allowed for lambda expressions using functional interface
         StringFunction ask = (s) -> s + "?";
-        printFormatted("Hello", exclaim);
-        printFormatted("Hello", ask);
+        printFormattedLambda("Oi", exclaim);
+        printFormattedLambda("Hello", ask);
     }
-    public static void printFormatted(String str, StringFunction format) {
+    public static void printFormattedLambda(String str, StringFunction format) {
         String result = format.run(str);
         System.out.println(result);
     }
